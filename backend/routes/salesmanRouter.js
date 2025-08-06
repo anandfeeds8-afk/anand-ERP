589eb187190715f7ace4eb2ad2763f2fefc8b929
Randeep Singh <randeeprajpal9@gmail.com>
2025-08-06 11:01:53 +0530

    createOrde changed to createOrder at salesmanRouter.js

--- backend/routes/salesmanRouter.js
+++ backend/routes/salesmanRouter.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const salesmanRouter = express.Router();
 const {
   loginSalesman,
@@ -6,22 +6,25 @@ const {
   getAllOrder,
   updatePayment,
   getOrderDetails,
-  deleteOrder
-  
-} = require('../controllers/Salesman');
+  deleteOrder,
+} = require("../controllers/Salesman");
 
-const { verifySalesmanToken } = require('../middleware/verifySalesman');
-const { createOrder } = require('../controllers/Orders');
+const { verifySalesmanToken } = require("../middleware/verifySalesman");
+const { createOrder } = require("../controllers/Orders");
 
 // Login
-salesmanRouter.post('/login', loginSalesman);
+salesmanRouter.post("/login", loginSalesman);
 
 // Protected routes
-salesmanRouter.post('/create_order', verifySalesmanToken, createOrde);
-salesmanRouter.delete('/delete_order/:orderId', verifySalesmanToken, deleteOrder);
-salesmanRouter.get('/get_allorder', verifySalesmanToken, getAllOrder);
-salesmanRouter.get('/orders/:orderId', verifySalesmanToken, getOrderDetails);
-salesmanRouter.get('/orders/due', verifySalesmanToken, getDueOrders);
-salesmanRouter.post('/orders/pay/:orderId', verifySalesmanToken, updatePayment);
+salesmanRouter.post("/create_order", verifySalesmanToken, createOrder);
+salesmanRouter.delete(
+  "/delete_order/:orderId",
+  verifySalesmanToken,
+  deleteOrder
+);
+salesmanRouter.get("/get_allorder", verifySalesmanToken, getAllOrder);
+salesmanRouter.get("/orders/:orderId", verifySalesmanToken, getOrderDetails);
+salesmanRouter.get("/orders/due", verifySalesmanToken, getDueOrders);
+salesmanRouter.post("/orders/pay/:orderId", verifySalesmanToken, updatePayment);
 
 module.exports = salesmanRouter;
